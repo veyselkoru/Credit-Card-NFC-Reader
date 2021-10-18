9c7847172742ab5cc8eb3dcf5d7a9bff0d5394ec
Aram <mkrtchyan.aram89@gmail.com>
2021-10-19 00:39:33 +0400

    Update CardNfcAsyncTask.java

--- creditCardNfcReader/src/main/java/com/pro100svitlo/creditCardNfcReader/CardNfcAsyncTask.java
+++ creditCardNfcReader/src/main/java/com/pro100svitlo/creditCardNfcReader/CardNfcAsyncTask.java
@@ -55,36 +55,7 @@ public class CardNfcAsyncTask extends AsyncTask<Void, Void, Object>{
     }
 
     public final static String CARD_UNKNOWN = EmvCardScheme.UNKNOWN.toString();
-    public final static String CARD_VISA = EmvCardScheme.VISA.toString();
-    public final static String CARD_NAB_VISA = EmvCardScheme.NAB_VISA.toString();
-    public final static String CARD_MASTER_CARD = EmvCardScheme.MASTER_CARD.toString();
-    public final static String CARD_AMERICAN_EXPRESS = EmvCardScheme.AMERICAN_EXPRESS.toString();
-    public final static String CARD_CB = EmvCardScheme.CB.toString();
-    public final static String CARD_LINK = EmvCardScheme.LINK.toString();
-    public final static String CARD_JCB = EmvCardScheme.JCB.toString();
-    public final static String CARD_DANKORT = EmvCardScheme.DANKORT.toString();
-    public final static String CARD_COGEBAN = EmvCardScheme.COGEBAN.toString();
-    public final static String CARD_DISCOVER = EmvCardScheme.DISCOVER.toString();
-    public final static String CARD_BANRISUL = EmvCardScheme.BANRISUL.toString();
-    public final static String CARD_SPAN = EmvCardScheme.SPAN.toString();
-    public final static String CARD_INTERAC = EmvCardScheme.INTERAC.toString();
-    public final static String CARD_ZIP = EmvCardScheme.ZIP.toString();
-    public final static String CARD_UNIONPAY = EmvCardScheme.UNIONPAY.toString();
-    public final static String CARD_EAPS = EmvCardScheme.EAPS.toString();
-    public final static String CARD_VERVE = EmvCardScheme.VERVE.toString();
-    public final static String CARD_TENN = EmvCardScheme.TENN.toString();
-    public final static String CARD_RUPAY = EmvCardScheme.RUPAY.toString();
-    public final static String CARD_ПРО100 = EmvCardScheme.ПРО100.toString();
-    public final static String CARD_ZKA = EmvCardScheme.ZKA.toString();
-    public final static String CARD_BANKAXEPT = EmvCardScheme.BANKAXEPT.toString();
-    public final static String CARD_BRADESCO = EmvCardScheme.BRADESCO.toString();
-    public final static String CARD_MIDLAND = EmvCardScheme.MIDLAND.toString();
-    public final static String CARD_PBS = EmvCardScheme.PBS.toString();
-    public final static String CARD_ETRANZACT = EmvCardScheme.ETRANZACT.toString();
-    public final static String CARD_GOOGLE = EmvCardScheme.GOOGLE.toString();
-    public final static String CARD_INTER_SWITCH = EmvCardScheme.INTER_SWITCH.toString();
-    public final static String CARD_MIR = EmvCardScheme.MIR.toString();
-    public final static String CARD_PROSTIR = EmvCardScheme.PROSTIR.toString();
+    public final static String CARD_VISA = EmvCardScheme.ANY_CARD.toString();
 
     private final static String NFC_A_TAG = "TAG: Tech [android.nfc.tech.IsoDep, android.nfc.tech.NfcA]";
     private final static String NFC_B_TAG = "TAG: Tech [android.nfc.tech.IsoDep, android.nfc.tech.NfcB]";
